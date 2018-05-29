a6f9b0188435a62f24b72b5a0a91d234d8af9bcf
= <=>
2018-05-29 11:20:31 +0200

    implemented account dao and person dao

--- BookStore/src/main/java/daoos/AccountDAOImplementation.java
+++ BookStore/src/main/java/daoos/AccountDAOImplementation.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import domain.Account;
+import domain.Book;
 import net.codejava.hibernate.SessionFactoryClass;
 
 public class AccountDAOImplementation {
@@ -23,18 +24,49 @@ public class AccountDAOImplementation {
 	}
 	
 	
-	public Account readAccount() {
-		return null;
-	}
-	
-	
-	public Account updateAccount() {
-		return null;
-	}
-	
-	
-	public void deleteAccount() {
+	public Account readAccount(long id) {
+		SessionFactory sessionFactory = sessionFactoryClass.setUp();
+		Session session = sessionFactory.openSession();
 		
+		Account account = session.get(Account.class, id);
+		sessionFactoryClass.exit();
+			
+		return account;
+	}
+	
+	
+	public Account updateAccount(Account account) {
+		SessionFactory sessionFactory = sessionFactoryClass.setUp();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		//updating the book in the database
+		session.update(account);
+		session.getTransaction().commit();
+		
+		//getting the updated book back from the database
+		account = readAccount(account.getId());
+		
+		session.close();
+		sessionFactoryClass.exit();
+		
+		return account;
+	}
+	
+	
+	public void deleteAccount(Account account) {
+		SessionFactory sessionFactory = sessionFactoryClass.setUp();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		session.delete(account);
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		sessionFactoryClass.exit();
+		
+		System.out.println("Account has been deleted");
 	}
 	
 }
--- BookStore/src/main/java/daoos/PersonDAOImplementation.java
+++ BookStore/src/main/java/daoos/PersonDAOImplementation.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import domain.Account;
+import domain.Book;
 import domain.Person;
 import net.codejava.hibernate.SessionFactoryClass;
 
@@ -21,5 +22,41 @@ public class PersonDAOImplementation {
 		session.getTransaction().commit();
 		session.close();
 		sessionFactoryClass.exit();
+	}
+	
+	public Person readReadPerson(long id) {
+		SessionFactory sessionFactory = sessionFactoryClass.setUp();
+		Session session = sessionFactory.openSession();
+		
+		Person person = session.get(Person.class, id);
+		sessionFactoryClass.exit();
+			
+		return person;
+	}
+	
+	public Person readPerson(long id) {
+		SessionFactory sessionFactory = sessionFactoryClass.setUp();
+		Session session = sessionFactory.openSession();
+		
+		Person person = session.get(Person.class, id);
+		sessionFactoryClass.exit();
+			
+		return person;
+	}
+	
+public void deletePerson(Person person) {
+		
+		SessionFactory sessionFactory = sessionFactoryClass.setUp();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		session.delete(person);
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		sessionFactoryClass.exit();
+		
+		System.out.println("Person has been deleted... at least in the database...");
 	}
 }
